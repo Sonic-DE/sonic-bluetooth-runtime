108eba5a93064208623e71b222f33ab0470ae8bc
Joseph Crowell <joseph.w.crowell@gmail.com>
2026-07-13 19:57:13 +1000

    src/kcm: remove wayland

--- src/kcm/bluetooth.cpp
+++ src/kcm/bluetooth.cpp
@@ -1,7 +1,8 @@
 /**
  * SPDX-FileCopyrightText: 2020 Nicolas Fella <nicolas.fella@gmx.de>
  * SPDX-FileCopyrightText: 2021 Nate Graham <nate@kde.org>
- * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR
+ * LicenseRef-KDE-Accepted-GPL
  */
 
 #include "bluetooth.h"
@@ -22,7 +23,6 @@
 #include <KLocalizedString>
 #include <KPluginFactory>
 #include <KSharedConfig>
-#include <KWaylandExtras>
 #include <KWindowSystem>
 
 #include <BluezQt/Services>
@@ -32,121 +32,120 @@
 
 #include "bluetoothmoduledata.h"
 
-K_PLUGIN_FACTORY_WITH_JSON(KCMBluetoothFactory, "kcm_bluetooth.json", registerPlugin<Bluetooth>(); registerPlugin<BluetoothModuleData>();)
+K_PLUGIN_FACTORY_WITH_JSON(KCMBluetoothFactory, "kcm_bluetooth.json",
+                           registerPlugin<Bluetooth>();
+                           registerPlugin<BluetoothModuleData>();)
 
 Bluetooth::Bluetooth(QObject *parent, const KPluginMetaData &data)
-    : KQuickConfigModule(parent, data)
-{
-    setButtons(Help);
+    : KQuickConfigModule(parent, data) {
+  setButtons(Help);
 
-    qmlRegisterAnonymousType<QAbstractItemModel>("org.kde.bluedevil.kcm", 1);
-    qmlRegisterSingletonInstance("org.kde.bluedevil.kcm", 1, 0, "FileReceiverSettings", FileReceiverSettings::self());
-    qmlRegisterSingletonInstance("org.kde.bluedevil.kcm", 1, 0, "GlobalSettings", GlobalSettings::self());
+  qmlRegisterAnonymousType<QAbstractItemModel>("org.kde.bluedevil.kcm", 1);
+  qmlRegisterSingletonInstance("org.kde.bluedevil.kcm", 1, 0,
+                               "FileReceiverSettings",
+                               FileReceiverSettings::self());
+  qmlRegisterSingletonInstance("org.kde.bluedevil.kcm", 1, 0, "GlobalSettings",
+                               GlobalSettings::self());
 }
 
-void Bluetooth::runWizard(QQuickItem *context)
-{
-    auto runIt = [this](const QString &windowHandle) {
-        auto *job = new KIO::CommandLauncherJob(QStringLiteral("bluedevil-wizard"), {QStringLiteral("--parentWindow"), windowHandle});
+void Bluetooth::runWizard(QQuickItem *context) {
+  auto runIt = [this](const QString &windowHandle) {
+    auto *job = new KIO::CommandLauncherJob(
+        QStringLiteral("bluedevil-wizard"),
+        {QStringLiteral("--parentWindow"), windowHandle});
 
-        connect(job, &KJob::finished, this, [this](KJob *job) {
-            if (job->error()) {
-                Q_EMIT errorOccured(job->errorString());
-            }
-        });
-        job->start();
-    };
-
-    QWindow *actualWindow = QQuickRenderControl::renderWindowFor(context->window());
-
-    // In full Qt Quick apps like plasma-settings, you just use the window directly
-    if (!actualWindow) {
-        actualWindow = context->window();
-    }
-
-    if (KWindowSystem::isPlatformWayland()) {
-        KWaylandExtras::exportWindow(actualWindow);
-        connect(
-            KWaylandExtras::self(),
-            &KWaylandExtras::windowExported,
-            this,
-            [runIt](QWindow *, const QString &windowHandle) {
-                runIt(windowHandle);
-            },
-            Qt::SingleShotConnection);
-    } else {
-        runIt(QStringLiteral("0x%1").arg((unsigned int)actualWindow->winId(), 0, 16));
-    }
-}
-
-void Bluetooth::runSendFile(const QString &ubi)
-{
-    auto *job = new KIO::CommandLauncherJob(QStringLiteral("bluedevil-sendfile"), {QStringLiteral("-u"), ubi});
-    job->setDesktopName(QStringLiteral("org.kde.bluedevilsendfile"));
     connect(job, &KJob::finished, this, [this](KJob *job) {
-        if (job->error()) {
-            Q_EMIT errorOccured(job->errorString());
-        }
+      if (job->error()) {
+        Q_EMIT errorOccured(job->errorString());
+      }
     });
     job->start();
+  };
+
+  QWindow *actualWindow =
+      QQuickRenderControl::renderWindowFor(context->window());
+
+  // In full Qt Quick apps like plasma-settings, you just use the window
+  // directly
+  if (!actualWindow) {
+    actualWindow = context->window();
+  }
+
+  runIt(QStringLiteral("0x%1").arg((unsigned int)actualWindow->winId(), 0, 16));
 }
 
-void Bluetooth::checkNetworkConnection(const QStringList &uuids, const QString &address)
-{
-    if (uuids.contains(BluezQt::Services::Nap)) {
-        checkNetworkInternal(QStringLiteral("nap"), address);
+void Bluetooth::runSendFile(const QString &ubi) {
+  auto *job = new KIO::CommandLauncherJob(QStringLiteral("bluedevil-sendfile"),
+                                          {QStringLiteral("-u"), ubi});
+  job->setDesktopName(QStringLiteral("org.kde.bluedevilsendfile"));
+  connect(job, &KJob::finished, this, [this](KJob *job) {
+    if (job->error()) {
+      Q_EMIT errorOccured(job->errorString());
     }
-
-    if (uuids.contains(BluezQt::Services::DialupNetworking)) {
-        checkNetworkInternal(QStringLiteral("dun"), address);
-    }
+  });
+  job->start();
 }
 
-void Bluetooth::checkNetworkInternal(const QString &service, const QString &address)
-{
-    QDBusMessage msg = QDBusMessage::createMethodCall(QStringLiteral("org.kde.plasmanetworkmanagement"),
-                                                      QStringLiteral("/org/kde/plasmanetworkmanagement"),
-                                                      QStringLiteral("org.kde.plasmanetworkmanagement"),
-                                                      QStringLiteral("bluetoothConnectionExists"));
+void Bluetooth::checkNetworkConnection(const QStringList &uuids,
+                                       const QString &address) {
+  if (uuids.contains(BluezQt::Services::Nap)) {
+    checkNetworkInternal(QStringLiteral("nap"), address);
+  }
 
-    msg << address;
-    msg << service;
-
-    QDBusPendingCallWatcher *call = new QDBusPendingCallWatcher(QDBusConnection::sessionBus().asyncCall(msg));
-    connect(call, &QDBusPendingCallWatcher::finished, this, [this, service, call]() {
-        QDBusPendingReply<bool> reply = *call;
-        if (reply.isError()) {
-            return;
-        }
-
-        Q_EMIT networkAvailable(service, reply.value());
-    });
+  if (uuids.contains(BluezQt::Services::DialupNetworking)) {
+    checkNetworkInternal(QStringLiteral("dun"), address);
+  }
 }
 
-void Bluetooth::setupNetworkConnection(const QString &service, const QString &address, const QString &deviceName)
-{
-    QDBusMessage msg = QDBusMessage::createMethodCall(QStringLiteral("org.kde.plasmanetworkmanagement"),
-                                                      QStringLiteral("/org/kde/plasmanetworkmanagement"),
-                                                      QStringLiteral("org.kde.plasmanetworkmanagement"),
-                                                      QStringLiteral("addBluetoothConnection"));
+void Bluetooth::checkNetworkInternal(const QString &service,
+                                     const QString &address) {
+  QDBusMessage msg = QDBusMessage::createMethodCall(
+      QStringLiteral("org.kde.plasmanetworkmanagement"),
+      QStringLiteral("/org/kde/plasmanetworkmanagement"),
+      QStringLiteral("org.kde.plasmanetworkmanagement"),
+      QStringLiteral("bluetoothConnectionExists"));
 
-    msg << address;
-    msg << service;
-    msg << i18nc("DeviceName Network (Service)", "%1 Network (%2)", deviceName, service);
+  msg << address;
+  msg << service;
 
-    QDBusConnection::sessionBus().call(msg, QDBus::NoBlock);
+  QDBusPendingCallWatcher *call =
+      new QDBusPendingCallWatcher(QDBusConnection::sessionBus().asyncCall(msg));
+  connect(call, &QDBusPendingCallWatcher::finished, this,
+          [this, service, call]() {
+            QDBusPendingReply<bool> reply = *call;
+            if (reply.isError()) {
+              return;
+            }
+
+            Q_EMIT networkAvailable(service, reply.value());
+          });
 }
 
-QString Bluetooth::receiveFolderPath() const
-{
-    return FileReceiverSettings::saveUrl().toLocalFile();
+void Bluetooth::setupNetworkConnection(const QString &service,
+                                       const QString &address,
+                                       const QString &deviceName) {
+  QDBusMessage msg = QDBusMessage::createMethodCall(
+      QStringLiteral("org.kde.plasmanetworkmanagement"),
+      QStringLiteral("/org/kde/plasmanetworkmanagement"),
+      QStringLiteral("org.kde.plasmanetworkmanagement"),
+      QStringLiteral("addBluetoothConnection"));
+
+  msg << address;
+  msg << service;
+  msg << i18nc("DeviceName Network (Service)", "%1 Network (%2)", deviceName,
+               service);
+
+  QDBusConnection::sessionBus().call(msg, QDBus::NoBlock);
 }
 
-void Bluetooth::setReceiveFolderPath(const QString &path)
-{
-    const QUrl url = QUrl::fromUserInput(path, QString(), QUrl::AssumeLocalFile);
-    FileReceiverSettings::setSaveUrl(url);
-    Q_EMIT receiveFolderPathChanged();
+QString Bluetooth::receiveFolderPath() const {
+  return FileReceiverSettings::saveUrl().toLocalFile();
+}
+
+void Bluetooth::setReceiveFolderPath(const QString &path) {
+  const QUrl url = QUrl::fromUserInput(path, QString(), QUrl::AssumeLocalFile);
+  FileReceiverSettings::setSaveUrl(url);
+  Q_EMIT receiveFolderPathChanged();
 }
 
 #include "bluetooth.moc"
